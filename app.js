8668cd7d2dbda98ef75fd8b8037e2afc80118737
‘Stephen <stephenlevesque2@gmail.com>
2023-01-12 14:02:05 -0700

    Snakes with JS

---
+++ app.js
@@ -0,0 +1,33 @@
+
+function greetUser(){
+    alert("Welcome to the Animal Kingdom!");
+}
+
+function Question(){
+    let favAnimal = prompt("What is your favorite animal?");
+    console.log("The user's favorite animal is: " + favAnimal);
+    if (favAnimal.toLowerCase() == "snakes") {
+
+        document.write("You've come to the right place!")
+    }
+    else { document.write("Sorry, but this page is about snakes;) ") }
+}
+
+
+
+
+
+
+
+
+function facts(){
+
+    document.write("Here, you'll find some fun facts about snakes!");
+}
+
+
+
+function scary(){
+
+    document.write("These ones are the scariest of them ALL!");
+}
